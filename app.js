b8104699f9f0a84658e61f447bf5889232a1c7f8
Elena Druzhinina <elenashebrova@gmail.com>
2022-03-29 12:57:24 +0700

    completed

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function handleFileSelect(e) {
   reader.readAsText(file);
 
   reader.onload = function () {
-    getResult(JSON.parse(reader.result));
+    formObj = JSON.parse(reader.result);
   };
 
   reader.onerror = function () {
@@ -28,8 +28,20 @@ function handleFileSelect(e) {
   };
 }
 
-function getResult(result) {
-  formObj = result;
+function onChangeColor() {
+  let checkboxColor = document.getElementById("Turnondarktheme?");
+  let colorValue = document.getElementById("colorValue").value;
+
+  if (checkboxColor.checked === true) {
+    document.body.style.setProperty("--var-color", colorValue);
+    checkboxColor.checked = false;
+  }
+}
+
+function turnThemeColor() {
+  let checkboxColor = document.getElementById("Turnondarktheme?");
+  if (!checkboxColor) return;
+  checkboxColor.addEventListener("change", onChangeColor);
 }
 
 function renderForm() {
@@ -43,21 +55,12 @@ function renderForm() {
     clearContainer();
   }
 
-  let fields = createFields();
-  let buttons = createButtons();
-  let fragment = formTemplate(fields, buttons);
+  let fields = fieldsTemplate();
+  let references = referencesTemplate();
+  let buttons = buttonsTemplate();
+  let fragment = formTemplate(fields, references, buttons);
   formContainer.insertAdjacentHTML("afterbegin", fragment);
-  addClassBootstrap();
-}
-
-function addClassBootstrap() {
-  let colorsType = document.querySelectorAll('input[type="color"]');
-  let colorsArray = Array.from(colorsType);
-  colorsArray.forEach((input) => input.classList.add("form-control-color"));
-
-  let checkboxType = document.querySelectorAll('input[type="checkbox"]');
-  let checkboxsArray = Array.from(checkboxType);
-  checkboxsArray.forEach((input) => input.classList.add("form-check-input"));
+  turnThemeColor();
 }
 
 function clearContainer() {
@@ -65,26 +68,94 @@ function clearContainer() {
   formContainer.innerHTML = "";
 }
 
-function createFields() {
+function fieldsTemplate() {
   let fragment = "";
   if (!formObj.fields) {
     return "";
   }
-
   formObj.fields.forEach((field) => {
-    if(field.input.type === 'textarea') {
+    if (field.input.type === "textarea") {
       fragment += `
       <div class="mb-3">
-        <label for="${field.label || ""}" class="form-label">${field.label || ""}</label>
-        <textarea class="form-control" id="${field.label || ""}" rows="3"></textarea>
+        <label class="form-label">${field.label || ""}</label>
+        <textarea class="form-control" rows="3"></textarea>
       </div>
     `;
-    }
-    else {
+    } else if (field.input.type === "color") {
+      let optionColors = "";
+      field.input.colors.forEach((item) => {
+        optionColors += `
+          <option value='${item}'>${item}</option>
+        `;
+      });
+      fragment += `
+      <label  class="form-label">Choose color scheme</label>
+      <input type="color" list="color" id="colorValue" class="form-control form-control-color" value="${field.input.colors[0]}" >
+      <datalist id="color">
+        ${optionColors}
+      </datalist>
+        <br>
+      `;
+    } else if (field.input.type === "technology") {
+      let technologies = "";
+      field.input.technologies.forEach((item) => {
+        technologies += `
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="${item}">
+            <label class="form-check-label" for="${item}">
+              ${item}
+            </label>
+          </div>
+        `;
+      });
+      fragment += `
+        <div class="mb-3">
+          <label class="form-label">
+            ${field.label || ""}
+          </label>
+          ${technologies}
+        </div>
+      `;
+    } else if (field.input.mask) {
+      field.id = Math.round(Math.random() * 1000);
+      $(function () {
+        $(`#${field.id}`).mask(`${field.input.mask}`);
+      });
+      fragment += `
+        <div class="mb-3">
+          <label for='${field.id}' class="form-label">
+            ${field.label || ""}
+          </label>
+          <input type="text" class="form-control" id='${field.id}' />
+        </div>
+      `;
+    } else if (field.input.type === "checkbox") {
+      fragment += `
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" id="${field.label
+          .split(" ")
+          .join("")}">
+        <label class="form-check-label">
+          ${field.label || ""}
+        </label>
+      </div>
+    `;
+    } else if (field.input.type === "file") {
+      fragment += `
+        <div class="mb-3">
+          <label class="form-label">${field.label || ""}</label>
+          <input class="form-control" type="file" accept="image/png, image/jpeg, .pdf" multiple>
+        </div>
+      `;
+    } else {
       fragment += `
       <div class="mb-3">
-        <label for="${field.label || ""}" class="form-label">${field.label || ""}</label>
-        <input type="${field.input.type}" class="form-control" placeholder='${field.input.placeholder || ""}' id="${field.label || ""}">
+        <label class="form-label">
+          ${field.label || ""}
+        </label>
+        <input type="${field.input.type}" class="form-control" placeholder='${
+        field.input.placeholder || ""
+      }' >
       </div>
     `;
     }
@@ -93,25 +164,65 @@ function createFields() {
   return fragment;
 }
 
-function createButtons() {
+function referencesTemplate() {
+  let fragment = "";
+  if (!formObj.references) {
+    return "";
+  }
+
+  let referncesWithCheckbox = formObj.references.filter((item) => item.input);
+  if (referncesWithCheckbox.length) {
+    formObj.references.forEach((reference) => {
+      if (!reference.text || !reference.ref) return;
+      fragment += `
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+          <div class='col-lg d-flex'>
+            <div >${reference.text} ${
+        reference["text without ref"] || ""
+      } </div>
+            <div class='link'><a href="#">${reference.ref}</a></div>
+          </div>
+        </div>
+      `;
+    });
+  } else {
+    formObj.references.forEach((reference) => {
+      if (!reference.text || !reference.ref) return;
+      fragment += `
+        <div class='col-lg d-flex'>
+          <div >${reference.text} ${reference["text without ref"] || ""} </div>
+          <div class='link'><a href="#">${reference.ref}</a></div>
+        </div>
+      `;
+    });
+  }
+  return fragment;
+}
+
+function buttonsTemplate() {
   let fragment = "";
   if (!formObj.buttons) {
     return "";
   }
   formObj.buttons.forEach((button) => {
     fragment += `
-    <button type="button" class="btn btn-primary mt-3">${button.text}</button>
+      <button type="button" class="btn btn-primary mt-3">${button.text}</button>
     `;
   });
   return fragment;
 }
 
-function formTemplate(fields, buttons) {
+function formTemplate(fields, references, buttons) {
   return `
     <h2 class="form-title mt-5 d-flex justify-content-center">${formObj.name}</h2>
     <form>
       ${fields}
+      <div class='row'>
+        ${references}
+      </div>
       ${buttons}
     </form>
+    <hr>
   `;
 }
